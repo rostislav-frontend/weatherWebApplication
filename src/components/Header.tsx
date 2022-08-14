57b476a488f7d24577ac9a0d05b2d4c51dafbc27
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-15 00:26:25 +0300

    added logo and link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,27 @@
-import { InputSearchCity } from "./InputSearchCity"
 
+import { Link } from "react-router-dom"
+import logo from "../icons/logo.svg"
+import { InputSearchCity } from "./InputSearchCity"
 
 
 export const Header = () => {
     return (
-        <header>
-            <div className="header-wrapper py-[20px] ">
-
+        <header className='flex py-[20px] justify-between items-center'>
+            <div className="header-logo flex items-center gap-2 text-[24px]">
+                <img src={logo} alt="" />
+                <span>Weather</span>
+            </div>
+            <div className="header-input ">
                 <InputSearchCity />
+            </div>
 
+            <div className="header-link flex gap-10">
+                <Link to='/'>
+                    Прогноз на 3 дня
+                </Link>
+                <Link to='/'>
+                    Прогноз на 5 дней
+                </Link>
             </div>
         </header>
     )
--- src/components/InputSearchCity.tsx
+++ src/components/InputSearchCity.tsx
@@ -11,29 +11,29 @@ export const InputSearchCity = () => {
     // custom hooks
     const debounced = useDebounce(searchCity)
 
-    const { isLoading, isError, data} = useSearchCityQuery(debounced, {
+    const { isLoading, isError, data } = useSearchCityQuery(debounced, {
         skip: debounced.length < 2,
         // обновление данных при фокусе на страницу
         // refetchOnFocus: true
     })
 
     console.log(data);
-    
+
 
 
     return (
-        <div className="input-header">
+        <>
             <input
-                className='border py-2 px-4 w-full h-[42px] mb-2 focus:outline-none'
+                className='border py-1 px-4  mb-2 focus:outline-none'
                 type="text"
                 placeholder="Введите свой город"
                 value={searchCity}
                 onChange={(event) => setSearchCity(event.target.value)}
             />
-            {isLoading && <h2>Идет загрузка</h2>}
+            {/* {isLoading && <h2>Идет загрузка</h2>} */}
+
+        </>
 
 
-
-        </div>
     )
 }
