058d48346b25e307b133d5275c50739e2ae80725
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-14 19:34:21 +0300

    fix taildind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+
+    screens: {
+      xs: '345px',
+      xsl: '430px',
+      // Intermediary
+      sm: '576px',
+      md: '672px',
+      
+      lg: '992px',
+      xl: '1170px',
+    },
+    
     extend: {},
   },
   plugins: [],
