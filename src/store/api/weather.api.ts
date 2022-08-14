ba90aa0a58d8fda5e11377bea398c9b059f66312
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-14 19:35:10 +0300

    added api conf weather

---
+++ src/store/api/weather.api.ts
@@ -0,0 +1,31 @@
+
+import { createApi, fetchBaseQuery }  from '@reduxjs/toolkit/query/react'
+
+export const weatherApi = createApi({
+    reducerPath: 'weatherApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://api.openweathermap.org/data/2.5/'
+      }),
+      
+    
+    endpoints: build => ({
+        
+        
+        // дженерики ts
+        searchCity: build.query<any, any>({
+            query: (search: string) => ({
+                url: `weather/`,
+
+                params: {
+                    q: search,
+                    appid: '3ab1a9998114dafe457fc2c22ae5badd'
+                    // per_page: 10
+                }
+            }),
+            transformResponse: (response: any) => response.items
+        }),
+    })
+})
+
+// get custom hooks 
+export const { useSearchCityQuery } = weatherApi
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { weatherApi } from "./api/weather.api";
 
 export const store = configureStore({
     reducer: {
+        [weatherApi.reducerPath] : weatherApi.reducer,
     },
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(weatherApi.middleware)
 })
+
+
+
+export type RootState = ReturnType<typeof store.getState>
