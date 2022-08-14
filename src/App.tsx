68a3961b9c98d2b6c839a29de06a16861a611ccb
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-14 19:34:42 +0300

    added react router app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import { CurrencyWeather } from "./components/CurrencyWeather";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="container mx-auto">
       <Header />
       <main>
         {/* основной контент */}
+        <Routes>
+          <Route path="/" element={<CurrencyWeather /> } />
+        </Routes>
       </main>
       <Footer />
     </div>
