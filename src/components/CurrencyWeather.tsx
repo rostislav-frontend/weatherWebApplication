cdb444f2744f3742a6d0ad1ed5738df872f6ce7d
rostislav <rostislav.plotnikov.04@mail.ru>
2022-08-15 00:47:21 +0300

    added fonts gilroy a

--- src/components/CurrencyWeather.tsx
+++ src/components/CurrencyWeather.tsx
@@ -1,8 +1,16 @@
-export const CurrencyWeather = () => (
 
-    <section className="currency-weather">
-        <h2>Текущая погода</h2>
-        
-    </section>
 
-)
+export const CurrencyWeather = () => {
+
+    return (
+        <section className="currency-weather pt-[50px]">
+            <div className="currency-weather__title">
+                <h2 className='text-center text-[32px] font-bold'>Текущая погода</h2>
+            </div>
+            <div className="currency-weather-card"></div>
+
+
+
+        </section>
+    )
+}
